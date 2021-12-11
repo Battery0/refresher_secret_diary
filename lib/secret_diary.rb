ba82a5d97a5eb696c1508db8be2d36a2bb58e1f3
ian <kickthechair@gmail.com>
2021-12-11 21:20:23 +0000

    diary lock test - pass

---
+++ lib/secret_diary.rb
@@ -0,0 +1,11 @@
+class SecretDiary
+
+  def initialize
+    @locked = true
+  end
+
+  def lock?
+    @locked = true
+  end
+
+end
---
+++ spec/secret_diary_spec.rb
@@ -0,0 +1,24 @@
+require 'secret_diary'
+
+describe SecretDiary do
+  
+  it '#lock? locks the diary' do
+    expect(subject.lock?).to eq(true)
+  end
+
+end
+
+
+
+
+# SecretDiary
+#   - lock
+#   - unlock
+#   - add_entry
+#   - get_entries
+
+# Initially the `SecretDiary` class is locked, meaning `add_entry` and `get_entries` should throw an error.
+
+# When the user calls `unlock`, `add_entry` and `get_entries` should work as desired.
+
+# When the user locks the diary, by calling `lock`, `add_entry` and `get_entries` should, again, throw an error.
